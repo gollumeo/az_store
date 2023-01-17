8906eb2a33432ba8c61f2290e5a99636eb410ded
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-01-17 15:25:57 +0100

    modif

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["*{html,js,php}"],
-=======
-  content: [
-    '*{html,js,php}'
-  ],
->>>>>>> origin/pierre
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> origin/pierre
