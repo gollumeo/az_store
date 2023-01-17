243a0d0932b4c0cb1599c303c8f6d97a4a92c8c8
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2023-01-17 15:18:42 +0100

    corrected names

--- cart.php
+++ cart.php
@@ -1,24 +1,5 @@
 <?php
-// include 'articles.php';
 session_start();
-if (isset($_POST['add_to_cart'])) {
-    $item_id = $_POST['item_id'];
-    $quantity = $_POST['quantity'];
-    if (!isset($_SESSION['cart'])) {
-        $_SESSION['cart'] = array();
-    }
-    if (!isset($_SESSION['cart'][$item_id])) {
-        // If the item isn't in the cart, add it with the specified quantity
-        $_SESSION['cart'][$item_id] = $quantity;
-    } else {
-        // If the item is already in the cart, update the quantity
-        $_SESSION['cart'][$item_id] += $quantity;
-    }
-}
-
-if (!isset($_SESSION['cart'])) {
-    $_SESSION['cart'] = array();
-}
 
 // Creating an array to store items to purchase
 $items = array(
@@ -28,52 +9,47 @@ $items = array(
     // ...
 );
 
-
-function add_to_cart($item_id, $quantity) {
-    if(!isset($_SESSION['cart'])) {
-        $_SESSION['cart'] = array();
-    }
-    if(!isset($_SESSION['cart'][$item_id])) {
-        // If the item isn't in the cart, add it with the specified quantity
-        $_SESSION['cart'][$item_id] = $quantity;
-    } else {
-        // If the item is already in the cart, update the quantity
-        $_SESSION['cart'][$item_id] += $quantity;
-    }
-
-    function remove_from_cart($item_id) {
-        if(isset($_SESSION['cart'][$item_id])) {
-            unset($_SESSION['cart'][$item_id]);
-        }
-    }
-
-    if(isset($_POST['remove_from_cart'])) {
-        $item_id = $_POST['item_id'];
-        remove_from_cart($item_id);
-    }
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = array();
 }
 
-if(isset($_POST['add_to_cart'])) {
+if (isset($_POST['add_to_cart'])) {
     $item_id = $_POST['item_id'];
     $quantity = $_POST['quantity'];
-    add_to_cart($item_id, $quantity);
+    if (isset($items[$item_id - 1])) {
+        if (!isset($_SESSION['cart'][$item_id])) {
+            // If the item isn't in the cart, add it with the specified quantity
+            $_SESSION['cart'][$item_id] = $quantity;
+        } else {
+            // If the item is already in the cart, update the quantity
+            $_SESSION['cart'][$item_id] += $quantity;
+        }
+    }
 }
 
+function remove_from_cart($item_id)
+{
+    if (isset($_SESSION['cart'][$item_id])) {
+        unset($_SESSION['cart'][$item_id]);
+    }
+}
+
+if (isset($_POST['remove_from_cart'])) {
+    $item_id = $_POST['item_id'];
+    remove_from_cart($item_id);
+}
 
 function get_cart_total()
 {
+    global $items;
     $total = 0;
-    foreach ($_SESSION['cart'] as $item_id => $quantity) {
-        // Get the price of the item from the database
-        $query = "SELECT price FROM items WHERE id = ?";
-        $stmt = $conn->prepare($query);
-        $stmt->bind_param("i", $item_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
-        $price = $row['price'];
-
-        $total += $price * $quantity;
+    if (isset($_SESSION['cart'])) {
+        foreach ($_SESSION['cart'] as $item_id => $quantity) {
+            $item = $items[$item_id - 1];
+            $price = $item['price'];
+            $item_total = $quantity * $price;
+            $total += $item_total;
+        }
     }
     return $total;
 }
@@ -89,28 +65,28 @@ function get_cart_total()
     </tr>
     <?php
     $total = 0;
-    foreach($_SESSION['cart'] as $item_id => $quantity) {
-        $item = $items[$item_id-1];
+    foreach ($_SESSION['cart'] as $item_id => $quantity) {
+        $item = $items[$item_id - 1];
         $name = $item['name'];
         $price = $item['price'];
         $item_total = $quantity * $price;
         $total += $item_total;
     ?>
-    <tr>
-        <td><?php echo $name; ?></td>
-        <td><?php echo $quantity; ?></td>
-        <td>$<?php echo $price; ?></td>
-        <td>$<?php echo $item_total; ?></td>
-        <td>
-            <form method="post" action="cart.php">
-                <input type="hidden" name="item_id" value="<?php echo $item_id; ?>">
-                <input type="submit" name="remove_from_cart" value="Remove">
-            </form>
-        </td>
-    </tr>
-    <? } ?>
+        <tr>
+            <td><?php echo $name; ?></td>
+            <td><?php echo $quantity; ?></td>
+            <td>$<?php echo $price; ?></td>
+            <td>$<?php echo $item_total; ?></td>
+            <td>
+                <form method="post" action="cart.php">
+                    <input type="hidden" name="item_id" value="<?php echo $item_id; ?>">
+                    <input type="submit" name="remove_from_cart" value="Remove">
+                </form>
+            </td>
+        </tr>
+    <?php } ?>
     <tr>
         <td colspan="3"></td>
-        <td>Total: €<?php echo $total ?></td>
+        <td>Total: $<?php echo get_cart_total(); ?></td>
     </tr>
 </table>
