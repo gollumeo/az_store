edeaf9038c9314d120a3a1f5e0cb41ca9cbb7290
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-01-17 14:21:03 +0100

    modif

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/output.css">
+    <title>Document</title>
+</head>
+
+<body class="w-screen min-h-screen space-y-16 flex flex-col items-center text-white bg-gradient-to-t from-slate-900 to-slate-500">
+    <header>
+        <div class="flex space-x-96 items-center w-screen justify-center">
+            <p>AZ[store]</p>
+            <nav>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Products</a>
+                <a href="">Contact</a>
+            </nav>
+            <img src="assets/resources/shopping-cart.svg" alt="Cart" width="30px" height="30px">
+        </div>
+    </header>
+    <main>
+        <div class="flex items-center justify-center space-x-32"> <!--BIG TITLE + SHOE -->
+            <div>
+                <p>SHOE THE RIGHT ONE.</p>
+                <button>See our store</button>
+            </div>
+            <div>
+                <img src="assets/resources/shoe_one.png" alt="nike" width="300px" height="300px">
+            </div>
+        </div>
+        <div class="flex justify-center"><!--SNEAKERS LIST-->
+            <div>
+                <img src="assets/resources/shoe_one.png" alt="" width="150px" height="150px">
+                <div>
+                    <div></div>
+                    <button></button>
+                </div>
+            </div>
+            <div>
+                <img src="assets/resources/shoe_one.png" alt="" width="150px" height="150px">
+                <div>
+                    <div></div>
+                    <button></button>
+                </div>
+            </div>
+            <div>
+                <img src="assets/resources/shoe_one.png" alt="" width="150px" height="150px">
+                <div>
+                    <div></div>
+                    <button></button>
+                </div>
+            </div>
+            <div>
+                <img src="assets/resources/shoe_one.png" alt="" width="150px" height="150px">
+                <div>
+                    <div></div>
+                    <button></button>
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-center"><!--PROVIDE THE BEST QUALITY-->
+            <img src="assets/resources/shoe_two.png" alt="" width="100px" height="100px">
+            <p></p>
+            <p></p>
+        </div>
+        <div class="flex justify-center space-x-28"><!--REVIEWS-->
+            <div>
+                <img src="assets/resources/image-emily.jpg" alt="">
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <img src="assets/resources/image-thomas.jpg" alt="">
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <img src="assets/resources/image-jennie.jpg" alt="">
+                <p></p>
+                <p></p>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <nav>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Products</a>
+            <a href="">Contact</a>
+        </nav>
+    </footer>
+</body>
+
+</html>
