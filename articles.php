6d4c73ab97c4054a018a8221094308e553623539
Pierre M <p.mauriello54@gmail.com>
2023-01-18 10:38:18 +0100

    shopping cart functionnality done

--- articles.php
+++ articles.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+</head>
+<body>
 <?php
 session_start();
 $articles = [
@@ -37,7 +46,7 @@ foreach ($articles as $item) {
     </form>
 
 
-<?
+<?php
 }
 
 if (!isset($_SESSION['cart'])) {
@@ -61,3 +70,5 @@ if (isset($_POST['add_to_cart']) && isset($_POST['item_id']) && array_key_exists
 }
 
 ?>
+</body>
+</html>
--- cart.php
+++ cart.php
@@ -1,8 +1,64 @@
 <?php
-// session_start();
+session_start();
 
 // Creating an array to store items to purchase
-include 'articles.php';
+$articles = [
+    [
+        "id" => 1,
+        "name" => "Nike 1",
+        "size" => "42",
+        "price" => "230"
+    ],
+    [
+        "id" => 2,
+        "name" => "Nike 2",
+        "size" => "40",
+        "price" => "230"
+    ],
+    [
+        "id" => 3,
+        "name" => "Nike 3",
+        "size" => "38",
+        "price" => "230"
+    ],
+    [
+        "id" => 4,
+        "name" => "Nike 4",
+        "size" => "44",
+        "price" => "230"
+    ],
+];
+
+foreach ($articles as $item) {
+?>
+
+    <form method="post" action="cart.php">
+        <input type="hidden" name="item_id" value="<?php echo $item['id']; ?>">
+        <input type="number" name="quantity" min="1" value="1">
+        <input type="submit" name="add_to_cart" value="Add to Cart">
+    </form>
+
+<?php
+}
+
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = array();
+}
+
+if (isset($_POST['add_to_cart']) && isset($_POST['item_id'])) {
+    $item_id = $_POST['item_id'];
+    $item = array_filter($articles, function ($item) use ($item_id) {
+        return $item['id'] == $item_id;
+    });
+    $item = reset($item);
+    $quantity = isset($_POST['quantity']) && !empty($_POST['quantity']) ? $_POST['quantity'] : 1;
+    $key = array_search($item, $articles);
+    if (!isset($_SESSION['cart'][$key])) {
+        $_SESSION['cart'][$key] = array("quantity" => $quantity, "price" => $item["price"], "name" => $item["name"]);
+    } else {
+        $_SESSION['cart'][$key]["quantity"] += $quantity;
+    }
+}
 
 if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = array();
@@ -26,10 +82,9 @@ function get_cart_total()
     global $articles;
     $total = 0;
     if (isset($_SESSION['cart'])) {
-        foreach ($_SESSION['cart'] as $item_id => $quantity) {
-            $item = $articles[$item_id - 1];
-            $price = $item['price'];
-            $item_total = intval($price) * intval($quantity);
+        foreach ($_SESSION['cart'] as $item_id => $item) {
+            $price = $item["price"];
+            $item_total = intval($price) * intval($item["quantity"]);
             $total += $item_total;
         }
     }
@@ -48,24 +103,25 @@ function get_cart_total()
     <?php
     $total = 0;
     if (isset($_SESSION['cart'])) {
-        foreach ($_SESSION['cart'] as $item_id => $quantity) {
-            $item = $articles[$item_id - 1];
-            $name = $item['name'];
-            if (!is_int($item['price'])) {
-                $item['price'] = intval($item['price']);
-                $quantity = intval($quantity);
-                $price = $item['price'] * $quantity;
-            }
-
-            $item_total = $price;
+        foreach ($_SESSION['cart'] as $item_id => $item) {
+            $name = $item["name"];
+            $quantity = $item["quantity"];
+            $price = $item["price"];
+            $item_total = $price * $quantity;
             $total += $item_total;
-
     ?>
             <tr>
-                <td><?php echo $name; ?></td>
-                <td><?php echo $quantity; ?></td>
-                <td>$<?php echo $price; ?></td>
-                <td>$<?php echo $item_total; ?></td>
+                <td>
+                </td>
+                <td>
+                    <?php echo $quantity; ?>
+                </td>
+                <td>
+                    €<?php echo $price; ?>
+                </td>
+                <td>
+                    €<?php echo $item_total; ?>
+                </td>
                 <td>
                     <form method="post" action="cart.php">
                         <input type="hidden" name="item_id" value="<?php echo $item_id; ?>">
@@ -73,10 +129,12 @@ function get_cart_total()
                     </form>
                 </td>
             </tr>
-    <?php }
-    } ?>
+    <?php
+        }
+    }
+    ?>
     <tr>
         <td colspan="3"></td>
-        <td>Total: $<?php echo get_cart_total(); ?></td>
+        <td>Total: €<?php echo get_cart_total(); ?></td>
     </tr>
 </table>
